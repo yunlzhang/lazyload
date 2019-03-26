00bf554420ef35347e33b8e73624c08015853041
zhangyunlong.zhang <zhangyunlong.zhang@qunar.com>
2019-03-26 16:40:07 +0800

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+
+import './polyfill';
+class Lazyload {
+    constructor(options){
+        this.setting = Object.assign({}, { 
+            src: 'data-src', 
+            selector: '.lazyload'
+        }, options);
+        this.images = document.querySelectorAll(this.setting.selector)
+        this.observer = null;
+        this._init();
+    }
+
+    _init(){
+        this.observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const target = entry.target
+                if (entry.intersectionRatio > 0) {
+                    const src = target.getAttribute(this.setting.src)
+                    if ('img' === target.tagName.toLowerCase()) {
+                        target.src = src
+                        target.removeAttribute(this.setting.src)
+                    } else {
+                        target.style.backgroundImage = `url(${src})`
+                    }
+                    this.observer.unobserve(target)
+                }
+            })
+        });
+        this.images.forEach(image => {
+            this.observer.observe(image)
+        })
+    }
+
+    update(){
+        this.images = document.querySelectorAll(this.setting.selector);
+        this.images.forEach(image => this.observer.observe(image))
+    }
+}
+
+export default Lazyload;
